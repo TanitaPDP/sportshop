6ae3cbecafbd29038adba810b45d0c136d38a509
TanitaPDP <tanita.pdp@gmail.com>
2019-08-19 11:09:43 +0300

    Add modal form on site

--- src/js/script.js
+++ src/js/script.js
@@ -51,6 +51,28 @@ $(document).ready(function(){
       };
       toggleSlide('.catalog-item__link');
       toggleSlide('.catalog-item__back');
+
+      // Modals
+      // Кнопки консультация получаем по дата атрибуту data-modal
+      $('[data-modal=consultation').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+      });
+      $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+      });
+
+      // Кнопки купить получаем по классу .button_mini
+      // $('.button_mini').on('click', function() {
+      //   $('.overlay, #order').fadeIn('slow');
+      // });
+      
+      // Вытаскиваем название товара из карточки и отображаем его в модальном окне заказа
+      $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+          $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+          $('.overlay, #order').fadeIn('slow');
+        })
+      });
   });
 
   
